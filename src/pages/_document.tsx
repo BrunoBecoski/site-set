d83b54b103979d44c6e84502df9b54ef4d60affc
Bruno Becoski <brunobecoski@gmail.com>
2026-04-02 19:56:41 -0300

    Instalando o Tailwind CSS na v3

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,9 +2,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="pt-br">
       <Head />
+      <title>Site.Set</title>
+      <link rel="icon"  href="/favicon.svg" type="image/svg+xml"/>
+
       <body className="antialiased">
+
         <Main />
         <NextScript />
       </body>
