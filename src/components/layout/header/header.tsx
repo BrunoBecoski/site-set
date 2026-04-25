fe90b4d3f4429dde1f82cdb52f04215720ff9215
Bruno Becoski <brunobecoski@gmail.com>
2026-04-25 19:43:14 -0300

    Atualiza o componente Header

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,3 +1,4 @@
+import { useCallback, useState } from "react";
 import Link from "next/link";
 import { Menu } from "lucide-react";
 
@@ -6,8 +7,14 @@ import { ActiveLink } from "../../active-link";
 import { Button } from "../../ui/button";
 
 export const Header = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleMenu = useCallback(() => {
+    setIsOpen(!isOpen)
+  }, [isOpen])
+
   return (
-    <header className="fixed top-0 z-50 w-full border-b border-white/10 bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60">
+    <header className="fixed top-0 z-50 w-full border-b border-white/10 backdrop-blur">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex h-20 md:h-24 items-center justify-between">
           <Logo />
@@ -15,17 +22,25 @@ export const Header = () => {
           <div className="flex items-center gap-6">
             <div className="hidden md:flex items-center gap-6">
               <ActiveLink href="/">Início</ActiveLink>
-            
               <ActiveLink href="/blog">Blog</ActiveLink>
             </div>
-            
+
             <Button variant="secondary" asChild>
               <Link href="/comecar">Começar</Link>
             </Button>
 
-            <Button variant="icon" size="icon" className="flex md:hidden">
-                <Menu className="size-5"/>
-            </Button>
+            <div className="flex md:hidden">
+              <Button variant="icon" size="icon" onClick={handleMenu} >
+                <Menu className="size-5" />
+              </Button>
+
+              {isOpen &&
+                <div className="flex gap-6 flex-col items-center fixed bg-gray-700 top-20 right-0 p-8 border-b border-white/10 backdrop-blur">
+                  <ActiveLink href="/">Início</ActiveLink>
+                  <ActiveLink href="/blog">Blog</ActiveLink>
+                </div>
+              }
+            </div>
           </div>
         </div>
       </div>
