4fc1af05e78adbb172c5a8df6bd0a92144deb140
Bruno Becoski <brunobecoski@gmail.com>
2026-05-06 16:34:53 -0300

    Atualiza o componente Header

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -16,55 +16,59 @@ export const Header = () => {
   }, [isOpen])
 
   return (
-    <header className="fixed top-0 z-50 w-full border-b border-white/10 backdrop-blur">
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div className="flex h-20 md:h-24 items-center justify-between">
-          <Link href="/" title="Página inicial">
-            <Image
-              src="/logo.svg"
-              alt="Site.Set"
-              width={115}
-              height={32}
-              className="hidden sm:block"
-            />
+    <>
+      <header className="fixed top-0 z-50 w-full border-b border-white/10 backdrop-blur">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="flex h-20 md:h-24 items-center justify-between">
+            <Link href="/" title="Página inicial">
+              <Image
+                src="/logo.svg"
+                alt="Site.Set"
+                width={115}
+                height={32}
+                className="hidden sm:block"
+              />
 
-            <Image
-              src="/icon.svg"
-              alt="Site.Set"
-              width={40}
-              height={40}
-              className="block sm:hidden"
-            />
-          </Link>
+              <Image
+                src="/icon.svg"
+                alt="Site.Set"
+                width={40}
+                height={40}
+                className="block sm:hidden"
+              />
+            </Link>
 
-          <div className="flex items-center gap-6">
-            <div className="hidden md:flex items-center gap-6">
-              <ActiveLink href="/">Início</ActiveLink>
-              <ActiveLink href="/blog">Blog</ActiveLink>
-            </div>
+            <div className="flex items-center gap-6">
+              <div className="hidden md:flex items-center gap-6">
+                <ActiveLink href="/">Início</ActiveLink>
+                <ActiveLink href="/blog">Blog</ActiveLink>
+              </div>
 
-            <Button variant="secondary" asChild>
-              <Link href="/comecar">Começar</Link>
-            </Button>
-
-            <div className="flex md:hidden">
-              <Button variant="icon" size="icon" onClick={handleMenu}>
-                {isOpen
-                  ? <X className="size-5" />
-                  : <Menu className="size-5" />
-                }
+              <Button variant="secondary" asChild>
+                <Link href="/comecar">Começar</Link>
               </Button>
 
-              {isOpen &&
-                <div className="flex gap-6 flex-col items-center fixed bg-gray-500/90 p-8 top-20 right-0 border-b border-l border-white/10 animate-opacity">
-                  <ActiveLink href="/">Início</ActiveLink>
-                  <ActiveLink href="/blog">Blog</ActiveLink>
-                </div>
-              }
+              <div className="flex md:hidden">
+                <Button variant="icon" size="icon" onClick={handleMenu}>
+                  {isOpen
+                    ? <X className="size-5" />
+                    : <Menu className="size-5" />
+                  }
+                </Button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
+
+      {isOpen &&
+        <div className="bg-gray-700/50 backdrop-blur top-0 bottom-0 left-0 right-0 fixed z-40 animate-opacity" onClick={handleMenu}>
+          <div className="flex gap-8 flex-col items-center fixed bg-gray-500 backdrop-blur  p-8 top-20 right-0 w-1/3 bottom-0 border-b border-l border-white/10 animate-opacity">
+            <ActiveLink href="/">Início</ActiveLink>
+            <ActiveLink href="/blog">Blog</ActiveLink>
+          </div>
+        </div>
+      }
+    </>
   )
 }
