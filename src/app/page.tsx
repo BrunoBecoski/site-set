b22d018fbcab108bcd4d3e58df2c1f98411fd074
Bruno Becoski <brunobecoski@gmail.com>
2026-05-08 17:29:44 -0300

    Melhora o SEO da LandingPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,30 @@
 import "@/styles/globals.css";
 
+import type { Metadata } from "next";
+
 import { LandingPage } from "@/templates/landing-page";
+
+export const metadata: Metadata = {
+  title: 'Site.Set',
+  description: 'Venda seus produtos como afiliado em um único lugar',
+  robots: 'index, follow',
+  openGraph: {
+    title: 'Site.Set',
+    description: 'Venda seus produtos como afiliado em um único lugar',
+    url: '/og-image.jpg',
+    siteName: 'Site.Set',
+    locale: 'pt_BR',
+    type: 'website',
+    images: [
+      {
+        url: '/og-image.jpg',
+        width: 800,
+        height: 600,
+        alt: 'Site.Set',
+      }
+    ]
+  }
+}
 
 export default function HomePage() {
   return (
