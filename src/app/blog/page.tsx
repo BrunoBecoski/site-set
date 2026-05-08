01c7428da99c796ca4bf5119b8e785cc6090b59e
Bruno Becoski <brunobecoski@gmail.com>
2026-05-08 18:58:11 -0300

    Melhora o SEO da página Blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,8 +1,31 @@
 import "@/styles/globals.css";
 
+import type { Metadata } from "next";
 import { allPosts } from "contentlayer/generated";
 
 import { BlogList } from "@/templates/blog";
+
+export const metadata: Metadata = {
+  title: 'Blog',
+  description: 'Dicas e estratégias para impulsionar seu negócio',
+  robots: 'index, follow',
+  openGraph: {
+    title: 'Site.Set',
+    description: 'Venda seus produtos como afiliado em um único lugar',
+    url: '/og-image.jpg',
+    siteName: 'Site.Set',
+    locale: 'pt_BR',
+    type: 'website',
+    images: [
+      {
+        url: '/og-image.jpg',
+        width: 800,
+        height: 600,
+        alt: 'Site.Set',
+      }
+    ]
+  }
+}
 
 export default function BlogListPage() {
   const sortedPosts = allPosts.sort(
